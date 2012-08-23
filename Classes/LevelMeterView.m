d7c649540799457b59fa8061c7228e34d7be85bc
Cubd <tylermilner@gmail.com>
2012-08-22 23:11:35 -0500

    Some more code cleanup.

--- Classes/LevelMeterView.m
+++ Classes/LevelMeterView.m
@@ -48,6 +48,4 @@
 	[self setNeedsDisplay];
 }
 
-
-
 @end
--- Classes/iPhoneStreamingPlayerViewController.m
+++ Classes/iPhoneStreamingPlayerViewController.m
@@ -30,7 +30,7 @@
 #import <CFNetwork/CFNetwork.h>
 
 
-@interface iPhoneStreamingPlayerViewController()
+@interface iPhoneStreamingPlayerViewController() <UITextFieldDelegate>
 
 @property (strong, nonatomic) IBOutlet UILabel *metadataArtistLabel;
 @property (strong, nonatomic) IBOutlet UILabel *metadataTitleLabel;
@@ -50,98 +50,39 @@
 @property (assign, nonatomic) BOOL uiIsVisible;
 
 - (void)setButtonImageNamed:(NSString *)imageName;
-- (void)destroyStreamer;
-- (void)createStreamer;
+- (void)spinButton;
+- (void)animationDidStop:(CAAnimation *)theAnimation finished:(BOOL)finished;
 - (IBAction)buttonPressed:(id)sender;
 - (IBAction)sliderMoved:(UISlider *)aSlider;
-- (void)spinButton;
-- (void)forceUIUpdate;
+- (void)destroyStreamer;
+- (void)createStreamer;
 - (void)createTimers:(BOOL)create;
+- (void)updateLevelMeters:(NSTimer *)timer;
 - (void)updateProgress:(NSTimer *)updatedTimer;
-- (void)playbackStateChanged:(NSNotification *)aNotification;
 - (void)applicationStateDidChange:(NSNotification *)notification;
+- (void)forceUIUpdate;
+- (void)playbackStateChanged:(NSNotification *)aNotification;
+- (void)presentAlertWithTitle:(NSNotification *)notification;
+#ifdef SHOUTCAST_METADATA
+- (void)metadataChanged:(NSNotification *)aNotification;
+#endif
+- (void)remoteControlReceivedWithEvent:(UIEvent *)event;
 
 @end
 
 
 @implementation iPhoneStreamingPlayerViewController
 
-//
-// setButtonImageNamed:
-//
-// Used to change the image on the playbutton. This method exists for
-// the purpose of inter-thread invocation because
-// the observeValueForKeyPath:ofObject:change:context: method is invoked
-// from secondary threads and UI updates are only permitted on the main thread.
-//
-// Parameters:
-//    imageNamed - the name of the image to set on the play button.
-//
-- (void)setButtonImageNamed:(NSString *)imageName
+#pragma mark - Dealloc
+- (void)dealloc
 {
-	if (!imageName)
-	{
-		imageName = @"playButton";
-	}
-	self.currentImageName = imageName;
-	
-	UIImage *image = [UIImage imageNamed:imageName];
-	
-	[self.button.layer removeAllAnimations];
-	[self.button setImage:image forState:UIControlStateNormal];
-		
-	if ([imageName isEqual:@"loadingbutton.png"])
-	{
-		[self spinButton];
-	}
-}
-
-//
-// destroyStreamer
-//
-// Removes the streamer, the UI update timer and the change notification
-//
-- (void)destroyStreamer
-{
-    if (_streamer)
-	{
-        [[NSNotificationCenter defaultCenter] removeObserver:self name:ASStatusChangedNotification object:_streamer];
-#ifdef SHOUTCAST_METADATA
-        [[NSNotificationCenter defaultCenter] removeObserver:self name:ASUpdateMetadataNotification object:_streamer];
-#endif
-        [self createTimers:NO];
-		[_streamer stop];
-		_streamer = nil;
-	}
-}
-
-
-
-//
-// createStreamer
-//
-// Creates or recreates the AudioStreamer object.
-//
-- (void)createStreamer
-{
-    if (_streamer)
-    {
-        return;
-    }
-    	
-	NSString *escapedValue = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(nil, (__bridge CFStringRef)self.downloadSourceField.text, NULL, NULL, kCFStringEncodingUTF8));
-
-	NSURL *url = [NSURL URLWithString:escapedValue];
-	_streamer = [[AudioStreamer alloc] initWithURL:url];
-	
-    [self createTimers:YES];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
     
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playbackStateChanged:) name:ASStatusChangedNotification object:_streamer];
-#ifdef SHOUTCAST_METADATA
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(metadataChanged:) name:ASUpdateMetadataNotification object:_streamer];
-#endif
+    [self createTimers:NO];
+	[self destroyStreamer];
 }
 
+#pragma mark - View Lifecycle
 //
 // viewDidLoad
 //
@@ -165,10 +106,52 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationStateDidChange:) name:UIApplicationDidBecomeActiveNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationStateDidChange:) name:UIApplicationDidEnterBackgroundNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(presentAlertWithTitle:) name:ASPresentAlertWithTitleNotification object:nil];
-
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+	[super viewDidAppear:animated];
+    
+	UIApplication *application = [UIApplication sharedApplication];
+	if([application respondsToSelector:@selector(beginReceivingRemoteControlEvents)])
+    {
+        [application beginReceivingRemoteControlEvents];
+    }
+	[self becomeFirstResponder]; // this enables listening for events
+	// update the UI in case we were in the background
+    [self forceUIUpdate];
+}
 
+#pragma mark - Button
+//
+// setButtonImageNamed:
+//
+// Used to change the image on the playbutton. This method exists for
+// the purpose of inter-thread invocation because
+// the observeValueForKeyPath:ofObject:change:context: method is invoked
+// from secondary threads and UI updates are only permitted on the main thread.
+//
+// Parameters:
+//    imageName - the name of the image to set on the play button.
+//
+- (void)setButtonImageNamed:(NSString *)imageName
+{
+	if (!imageName)
+	{
+		imageName = @"playButton";
+	}
+	self.currentImageName = imageName;
+	
+	UIImage *image = [UIImage imageNamed:imageName];
+	
+	[self.button.layer removeAllAnimations];
+	[self.button setImage:image forState:UIControlStateNormal];
+		
+	if ([imageName isEqual:@"loadingbutton.png"])
+	{
+		[self spinButton];
+	}
+}
 
 //
 // spinButton
@@ -184,11 +167,11 @@
 	self.button.layer.anchorPoint = CGPointMake(0.5f, 0.5f);
 	self.button.layer.position = CGPointMake(frame.origin.x + 0.5f * frame.size.width, frame.origin.y + 0.5f * frame.size.height);
 	[CATransaction commit];
-
+    
 	[CATransaction begin];
 	[CATransaction setValue:(id)kCFBooleanFalse forKey:kCATransactionDisableActions];
 	[CATransaction setValue:[NSNumber numberWithFloat:2.0f] forKey:kCATransactionAnimationDuration];
-
+    
 	CABasicAnimation *animation;
 	animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
 	animation.fromValue = [NSNumber numberWithFloat:0.0f];
@@ -196,7 +179,7 @@
 	animation.timingFunction = [CAMediaTimingFunction functionWithName: kCAMediaTimingFunctionLinear];
 	animation.delegate = self;
 	[self.button.layer addAnimation:animation forKey:@"rotationAnimation"];
-
+    
 	[CATransaction commit];
 }
 
@@ -218,6 +201,7 @@
 	}
 }
 
+#pragma mark - IBActions
 //
 // buttonPressed:
 //
@@ -261,6 +245,184 @@
 	}
 }
 
+#pragma mark - AudioStreamer Lifecycle
+//
+// destroyStreamer
+//
+// Removes the streamer, the UI update timer and the change notification
+//
+- (void)destroyStreamer
+{
+    if (_streamer)
+	{
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:ASStatusChangedNotification object:_streamer];
+#ifdef SHOUTCAST_METADATA
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:ASUpdateMetadataNotification object:_streamer];
+#endif
+        [self createTimers:NO];
+		[_streamer stop];
+		_streamer = nil;
+	}
+}
+
+//
+// createStreamer
+//
+// Creates or recreates the AudioStreamer object.
+//
+- (void)createStreamer
+{
+    if (_streamer)
+    {
+        return;
+    }
+    	
+	NSString *escapedValue = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(nil, (__bridge CFStringRef)self.downloadSourceField.text, NULL, NULL, kCFStringEncodingUTF8));
+
+	NSURL *url = [NSURL URLWithString:escapedValue];
+	_streamer = [[AudioStreamer alloc] initWithURL:url];
+	
+    [self createTimers:YES];
+    
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playbackStateChanged:) name:ASStatusChangedNotification object:_streamer];
+#ifdef SHOUTCAST_METADATA
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(metadataChanged:) name:ASUpdateMetadataNotification object:_streamer];
+#endif
+}
+
+#pragma mark - NSTimers
+//
+// createTimers
+//
+// Creates or destoys the timers
+//
+-(void)createTimers:(BOOL)create
+{
+	if (create)
+    {
+		if (self.streamer)
+        {
+            [self createTimers:NO];
+            self.progressUpdateTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(updateProgress:) userInfo:nil repeats:YES];
+            self.levelMeterUpdateTimer = [NSTimer scheduledTimerWithTimeInterval:.1 target:self selector:@selector(updateLevelMeters:) userInfo:nil repeats:YES];
+		}
+	}
+	else
+    {
+		if (self.progressUpdateTimer)
+		{
+			[self.progressUpdateTimer invalidate];
+			self.progressUpdateTimer = nil;
+		}
+		if (self.levelMeterUpdateTimer)
+        {
+			[self.levelMeterUpdateTimer invalidate];
+			self.levelMeterUpdateTimer = nil;
+		}
+	}
+}
+
+//
+// updateLevelMeters:
+//
+- (void)updateLevelMeters:(NSTimer *)timer
+{
+    if ([self.streamer isMeteringEnabled] && self.uiIsVisible)
+    {
+        [self.levelMeterView updateMeterWithLeftValue:[self.streamer averagePowerForChannel:0] rightValue:[self.streamer averagePowerForChannel:([self.streamer numberOfChannels] > 1 ? 1 : 0)]];
+    }
+}
+
+//
+// updateProgress:
+//
+// Invoked when the AudioStreamer
+// reports that its playback progress has changed.
+//
+- (void)updateProgress:(NSTimer *)updatedTimer
+{
+	if (self.streamer.bitRate != 0.0)
+	{
+		double progress = self.streamer.progress;
+		double duration = self.streamer.duration;
+        
+		if (duration > 0)
+		{
+			[self.positionLabel setText:[NSString stringWithFormat:@"Time Played: %.1f/%.1f seconds", progress, duration]];
+			[self.progressSlider setEnabled:YES];
+			[self.progressSlider setValue:100 * progress / duration];
+		}
+		else
+		{
+			[self.progressSlider setEnabled:NO];
+		}
+	}
+	else
+	{
+		self.positionLabel.text = @"Time Played:";
+	}
+}
+
+#pragma mark - UITextFieldDelegate
+//
+// textFieldShouldReturn:
+//
+// Dismiss the text field when done is pressed
+//
+// Parameters:
+//    sender - the text field
+//
+// returns YES
+//
+- (BOOL)textFieldShouldReturn:(UITextField *)sender
+{
+	[sender resignFirstResponder];
+	[self createStreamer];
+	return YES;
+}
+
+#pragma mark - UI Updates
+- (void)applicationStateDidChange:(NSNotification *)notification
+{
+    if ([notification.name isEqualToString:UIApplicationDidBecomeActiveNotification])
+    {
+        self.uiIsVisible = YES;
+    }
+    else if ([notification.name isEqualToString:UIApplicationDidEnterBackgroundNotification])
+    {
+        self.uiIsVisible = NO;
+    }
+}
+
+//
+// forceUIUpdate
+//
+// When foregrounded force UI update since we didn't update in the background
+//
+-(void)forceUIUpdate
+{
+	if (self.currentArtist)
+    {
+        self.metadataArtistLabel.text = self.currentArtist;
+    }
+	if (self.currentTitle)
+    {
+        self.metadataTitleLabel.text = self.currentTitle;
+    }
+    
+	if (!self.streamer)
+    {
+		[self.levelMeterView updateMeterWithLeftValue:0.0
+                                           rightValue:0.0];
+		[self setButtonImageNamed:@"playbutton.png"];
+	}
+	else
+    {
+        [self playbackStateChanged:nil];
+    }
+}
+
+#pragma mark - AudioStreamer Notifications
 //
 // playbackStateChanged:
 //
@@ -306,61 +468,17 @@
     }
 }
 
-
-
-//
-// updateProgress:
-//
-// Invoked when the AudioStreamer
-// reports that its playback progress has changed.
-//
-- (void)updateProgress:(NSTimer *)updatedTimer
+- (void)presentAlertWithTitle:(NSNotification *)notification
 {
-	if (self.streamer.bitRate != 0.0)
-	{
-		double progress = self.streamer.progress;
-		double duration = self.streamer.duration;
-        
-		if (duration > 0)
-		{
-			[self.positionLabel setText:[NSString stringWithFormat:@"Time Played: %.1f/%.1f seconds", progress, duration]];
-			[self.progressSlider setEnabled:YES];
-			[self.progressSlider setValue:100 * progress / duration];
-		}
-		else
-		{
-			[self.progressSlider setEnabled:NO];
-		}
-	}
-	else
-	{
-		self.positionLabel.text = @"Time Played:";
-	}
-}
-
-//
-// textFieldShouldReturn:
-//
-// Dismiss the text field when done is pressed
-//
-// Parameters:
-//    sender - the text field
-//
-// returns YES
-//
-- (BOOL)textFieldShouldReturn:(UITextField *)sender
-{
-	[sender resignFirstResponder];
-	[self createStreamer];
-	return YES;
-}
-
-- (void)dealloc
-{
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    NSString *title = [[notification userInfo] objectForKey:@"title"];
+    NSString *message = [[notification userInfo] objectForKey:@"message"];
     
-    [self createTimers:NO];
-	[self destroyStreamer];
+    dispatch_queue_t main_queue = dispatch_get_main_queue();
+    
+    dispatch_async(main_queue, ^{
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", @"") otherButtonTitles: nil];
+        [alert show];
+    });
 }
 
 #ifdef SHOUTCAST_METADATA
@@ -434,99 +552,7 @@
 }
 #endif
 
-//
-// updateLevelMeters:
-//
-
-- (void)updateLevelMeters:(NSTimer *)timer
-{
-    if ([self.streamer isMeteringEnabled] && self.uiIsVisible)
-    {
-        [self.levelMeterView updateMeterWithLeftValue:[self.streamer averagePowerForChannel:0] rightValue:[self.streamer averagePowerForChannel:([self.streamer numberOfChannels] > 1 ? 1 : 0)]];
-    }
-}
-
-
-
-//
-// forceUIUpdate
-//
-// When foregrounded force UI update since we didn't update in the background
-//
--(void)forceUIUpdate
-{
-	if (self.currentArtist)
-    {
-        self.metadataArtistLabel.text = self.currentArtist;
-    }
-	if (self.currentTitle)
-    {
-        self.metadataTitleLabel.text = self.currentTitle;
-    }
-    
-	if (!self.streamer)
-    {
-		[self.levelMeterView updateMeterWithLeftValue:0.0
-									  rightValue:0.0];
-		[self setButtonImageNamed:@"playbutton.png"];
-	}
-	else
-    {
-        [self playbackStateChanged:nil];
-    }
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-	[super viewDidAppear:animated];
-    
-	UIApplication *application = [UIApplication sharedApplication];
-	if([application respondsToSelector:@selector(beginReceivingRemoteControlEvents)])
-    {
-        [application beginReceivingRemoteControlEvents];
-    }
-	[self becomeFirstResponder]; // this enables listening for events
-	// update the UI in case we were in the background
-    [self forceUIUpdate];
-}
-
-- (BOOL)canBecomeFirstResponder
-{
-	return YES;
-}
-
-//
-// createTimers
-//
-// Creates or destoys the timers
-//
--(void)createTimers:(BOOL)create
-{
-	if (create)
-    {
-		if (self.streamer)
-        {
-            [self createTimers:NO];
-            self.progressUpdateTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(updateProgress:) userInfo:nil repeats:YES];
-            self.levelMeterUpdateTimer = [NSTimer scheduledTimerWithTimeInterval:.1 target:self selector:@selector(updateLevelMeters:) userInfo:nil repeats:YES];
-		}
-	}
-	else
-    {
-		if (self.progressUpdateTimer)
-		{
-			[self.progressUpdateTimer invalidate];
-			self.progressUpdateTimer = nil;
-		}
-		if (self.levelMeterUpdateTimer)
-        {
-			[self.levelMeterUpdateTimer invalidate];
-			self.levelMeterUpdateTimer = nil;
-		}
-	}
-}
-
-#pragma mark Remote Control Events
+#pragma mark - Remote Control Events
 /* The iPod controls will send these events when the app is in the background */
 - (void)remoteControlReceivedWithEvent:(UIEvent *)event
 {
@@ -549,29 +575,9 @@
 	}
 }
 
-- (void)applicationStateDidChange:(NSNotification *)notification
+- (BOOL)canBecomeFirstResponder
 {
-    if ([notification.name isEqualToString:UIApplicationDidBecomeActiveNotification])
-    {
-        self.uiIsVisible = YES;
-    }
-    else if ([notification.name isEqualToString:UIApplicationDidEnterBackgroundNotification])
-    {
-        self.uiIsVisible = NO;
-    }
-}
-
-- (void)presentAlertWithTitle:(NSNotification *)notification
-{
-    NSString *title = [[notification userInfo] objectForKey:@"title"];
-    NSString *message = [[notification userInfo] objectForKey:@"message"];
-    
-    dispatch_queue_t main_queue = dispatch_get_main_queue();
-    
-    dispatch_async(main_queue, ^{
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", @"") otherButtonTitles: nil];
-        [alert show];
-    });
+	return YES;
 }
 
 @end
